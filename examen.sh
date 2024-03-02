03a981307067239bbf80eb19c39e659be21cc38a
Karenmtz145 <156533044+Karenmtz145@users.noreply.github.com>
2024-03-02 10:42:44 -0600

    Add files via upload

---
+++ examen.sh
@@ -0,0 +1,33 @@
+1. Crea un script con el documento Accidentes_ags_2021.csv para que sume los datos de OTROVEHIC
+Solo se tiene que mostrar el número:
+
+#!/bin/bash
+suma=$(awk -F ',' '{sum+=$21} END{print sum}' Accidentes_ags_2021.csv)
+echo "LA SUMA DE 'VEHICULO' ES: $suma"
+__________________________________________________________________________________________________________
+
+2. Crea un script con el documento Accidentes_ags_2021.csv para que muestre los datos ID y FERROCARRI cuando sean en constitucion
+El formato en que se tiene que mostrar es en columnas
+
+Ejemplo
+
+Pagina                        Estatus
+www.google.com        OK
+plataforma-utslp.net   OK
+sito.utslp.edu.mx        OK
+www.mozilla.org         OK
+git-scm.com                OK
+omar.                          FAIL
+
+
+R=
+
+#!/bin/bash
+archivo="Accidentes_ags_2021.csv"
+
+echo "ID,           FERROCARRI"
+echo "------------------------"
+grep -i "CONSTITUCION" "$archivo" | awk -F ',' '{print $1" "$22}' | column -s', ' -t
+
+
+
